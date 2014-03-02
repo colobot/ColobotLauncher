1e56fbe00a8bfe286a8068b53c4847ab7760569d
Emxx <emxx52@gmail.com>
2014-03-02 17:37:43 +0100

    Removed splashscreen
    This commit reverts d47cfc98f71836f8328bd1d30effcbe152f12c4c

--- main.cpp
+++ main.cpp
@@ -3,13 +3,10 @@
 #include <string>
 #include <map>
 #include <windows.h>
-#include <gdiplus.h>
-#include "SplashWnd.h"
 
 #include "utils.h"
 
 using namespace std;
-using namespace Gdiplus;
 
 HWND hwnd = NULL;
 
@@ -30,6 +27,15 @@ void ReportError(std::string text)
     MessageBox(hwnd, ("There was a problem starting your game.\nError message: "+text).c_str(), "ColobotLauncher - ERROR", MB_ICONERROR | MB_OK);
 }
 
+void ReportError(std::string text, int code)
+{
+    cout << "ERROR: " << text << ", code: " << code << endl;
+    char* codeStr = new char[4];
+    sprintf(codeStr, "%d", code);
+    MessageBox(hwnd, ("There was a problem starting your game.\nError message: "+text+"\nError code: "+codeStr).c_str(), "ColobotLauncher - ERROR", MB_ICONERROR | MB_OK);
+    delete codeStr;
+}
+
 // --------
 
 std::string colobotDir;
@@ -37,7 +43,6 @@ std::string colobotDir;
 enum ColobotVersion {
 	VERSION_UNKNOWN,
 	VERSION_1_9_PL,
-	VERSION_1_9_PL_KS,
 	VERSION_1_3_EN,
 	VERSION_1_8_EN
 };
@@ -55,11 +60,11 @@ std::map<ColobotVersion, VersionData> versions;
 void LoadVersions()
 {
 	//TODO: Auto-update? Reading from file?
-	
+
 	VersionData version_unknown;
 	version_unknown.fullVersionStr = "Colobot Orginal (unknown version)";
 	versions[VERSION_UNKNOWN] = version_unknown;
-	
+
 	VersionData version_1_9_pl;
 	version_1_9_pl.md5 = "991ff670d594e064333bf8b3ef14a395";
 	version_1_9_pl.websiteStringPtr = (void*)0x0053CEE0;
@@ -67,15 +72,7 @@ void LoadVersions()
 	version_1_9_pl.versionStr = "1.9 PL";
 	version_1_9_pl.fullVersionStr = "Colobot Orginal 1.9 PL";
 	versions[VERSION_1_9_PL] = version_1_9_pl;
-	
-	VersionData version_1_9_pl_ks;
-	version_1_9_pl_ks.md5 = "83e54ee377bcdf9f4930e0892952c4ab";
-	version_1_9_pl_ks.websiteStringPtr = (void*)0x0053CEE8;
-	version_1_9_pl_ks.versionStringPtr = (void*)0x005416B0;
-	version_1_9_pl_ks.versionStr = "1.9 KŒ";
-	version_1_9_pl_ks.fullVersionStr = "Colobot Orginal 1.9 PL (Komputer Œwiat edition)";
-	versions[VERSION_1_9_PL_KS] = version_1_9_pl_ks;
-	
+
 	VersionData version_1_3_en;
 	version_1_3_en.md5 = "19b10cab2e24170e75791e0d8ef6adb2";
 	version_1_3_en.websiteStringPtr = (void*)0x00537790;
@@ -83,7 +80,7 @@ void LoadVersions()
 	version_1_3_en.versionStr = "1.3 EN";
 	version_1_3_en.fullVersionStr = "Colobot Orginal 1.3 EN";
 	versions[VERSION_1_3_EN] = version_1_3_en;
-	
+
 	VersionData version_1_8_en;
 	version_1_8_en.md5 = "60b187eb8caa297af90fb9f1c618f9b0";
 	version_1_8_en.websiteStringPtr = (void*)0x0053CEE8;
@@ -105,81 +102,49 @@ ColobotVersion GetColobotVersion()
 	return VERSION_UNKNOWN;
 }
 
-inline int GetPercentage(int stage)
-{
-	return (int)((((double)stage)/7)*100);
-}
-
 int main(int argc, char *argv[])
 {
 	LoadVersions();
-	
+
 	CHAR* workingDir = new CHAR[513];
 	GetCurrentDirectory(512, workingDir);
-	
+
 	if(!fexists(std::string(workingDir)+"\\colobot.exe")) {
 		//TODO: Search in Program Files
 		ReportError("colobot.exe not found!");
 		return EXIT_FAILURE;
 	}
-	
+
     colobotDir = std::string(workingDir)+"\\";
     
-	if(argc >= 2) {
-		if(!strcmp(argv[1], "getmd5")) {
-			cout << "MD5: " << GetFileMD5(colobotDir+"colobot.exe") << endl;
-			MessageBox(hwnd, ("MD5: "+GetFileMD5(colobotDir+"colobot.exe")).c_str(), "ColobotLauncher", MB_ICONINFORMATION | MB_OK);
-			return EXIT_SUCCESS;
-		}
-		
-		if(!strcmp(argv[1], "getclassname")) {
-			hwnd = FindWindow(NULL, "COLOBOT");
-			char* classname = new char[64];
-			GetClassName(hwnd, classname, 64);
-			
-			cout << "Class name: " << classname << endl;
-			MessageBox(hwnd, (std::string("Class name: ")+classname).c_str(), "ColobotLauncher", MB_ICONINFORMATION | MB_OK);
-			return EXIT_SUCCESS;
+    if(argc >= 2) {
+    	if(!strcmp(argv[1], "getmd5")) {
+	    	cout << "MD5: " << GetFileMD5(colobotDir+"colobot.exe") << endl;
+	    	return EXIT_SUCCESS;
 		}
     }
     
-    GdiplusStartupInput gdiplusStartupInput;
-	ULONG_PTR gdiplusToken;
-	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-	
-	WCHAR* splashFile = L"ppc.png";
-	Image* image = Image::FromFile(splashFile);
-	
-	CSplashWnd splash;
-	splash.SetImage(image);
-	splash.Show();
-	splash.SetProgress(GetPercentage(0), "Welcome!");
-	Sleep(1500);
-    
     cout << "Setting combatibility mode... ";
-	splash.SetProgress(GetPercentage(1), "Setting combatibility mode...");
     HKEY hkey;
     LONG res = RegOpenKeyEx(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers", 0, KEY_ALL_ACCESS, &hkey);
     if(res != ERROR_SUCCESS) {
-           ReportError("opening key");
+           ReportError("opening key", res);
            return EXIT_FAILURE;
     }
     LPCTSTR data = "WINXPSP3 RUNASADMIN\0";
     res = RegSetValueEx(hkey, (LPCTSTR)((colobotDir+"colobot.exe").c_str()), 0, REG_SZ, (LPBYTE)data, strlen(data)+1);
     if(res != ERROR_SUCCESS) {
-           ReportError("writing value");
+           ReportError("writing value", res);
            return EXIT_FAILURE;
     }
     cout << "OK" << endl;
     
     cout << "Checking version... ";
-	splash.SetProgress(GetPercentage(2), "Checking version...");
 	ColobotVersion version = GetColobotVersion();
 	VersionData versionData = versions[version];
 	cout << versionData.fullVersionStr << endl;
     
     cout << "Creating process... ";
-	splash.SetProgress(GetPercentage(3), "Creating process...");
     PROCESS_INFORMATION pi;
     if(!StartProcess(colobotDir.c_str(), (colobotDir+"colobot.exe -nocd").c_str(), &pi)) {
         ReportError("creating process");
@@ -189,14 +154,12 @@ int main(int argc, char *argv[])
     cout << "OK" << endl;
     
     cout << "Waiting for game window... ";
-	splash.SetProgress(GetPercentage(4), "Waiting for game window...");
     while(hwnd == NULL) {
-    	hwnd = FindWindow("D3D Window", "COLOBOT");
+    	hwnd = FindWindow(NULL, "COLOBOT");
     }
     cout << "OK" << endl;
     
     cout << "Changing window title... ";
-	splash.SetProgress(GetPercentage(5), "Changing window title...");
     if(!SetWindowText(hwnd, versionData.fullVersionStr.c_str())) {
     	cout << "ERROR (" << GetLastError() << "), SKIP" << endl;
     } else {
@@ -204,7 +167,6 @@ int main(int argc, char *argv[])
     }
     
     cout << "Changing website text... ";
-	splash.SetProgress(GetPercentage(6), "Changing website text...");
     if(version == VERSION_UNKNOWN || versionData.websiteStringPtr == NULL) {
     	cout << "SKIP" << endl;
     } else {
@@ -218,7 +180,6 @@ int main(int argc, char *argv[])
     }
     
     cout << "Changing version text... ";
-	splash.SetProgress(GetPercentage(7), "Changing website text...");
     if(version == VERSION_UNKNOWN || versionData.versionStringPtr == NULL) {
     	cout << "SKIP" << endl;
     } else {
@@ -231,14 +192,8 @@ int main(int argc, char *argv[])
     	}
     }
     
-    splash.Hide();
-    delete image;
-    
     CloseHandle(process);
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
-    
-    GdiplusShutdown(gdiplusToken);
-    
     return EXIT_SUCCESS;
 }
